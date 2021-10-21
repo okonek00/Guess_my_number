0b890b5a54355b08ed8e518371f86e2b5bfdb7ba
okonek00 <73000139+okonek00@users.noreply.github.com>
2021-10-21 22:35:36 +0200

    Code cleaning

--- script.js
+++ script.js
@@ -1,63 +1,60 @@
 'use strict';
-
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.message').textContent = '🎉 Correct Number!';
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-
 let score = 20;
-
 let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+const displayNumber = function (number) {
+  document.querySelector('.number').textContent = number;
+};
+
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+
+const displayHighscore = function (highscore) {
+  document.querySelector('.highscore').textContent = highscore;
+};
+
+const setBackgroundColor = function (color) {
+  document.querySelector('body').style.backgroundColor = color;
+};
+
+const numberStyle = function (width) {
+  document.querySelector('.number').style.width = width;
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
   // When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No number!';
+    displayMessage('⛔ No number!');
 
     // When player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
-
-    document.querySelector('body').style.backgroundColor = 'green';
-
-    document.querySelector('.number').style.width = '40rem';
-
-    document.querySelector('.number').textContent = secretNumber;
+    displayMessage('🎉 Correct Number!');
+    setBackgroundColor('green');
+    numberStyle('40rem');
+    displayNumber(secretNumber);
 
     if (score > highscore) {
       highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
+      displayHighscore(highscore);
     }
 
-    // When guess is to high
-  } else if (guess > secretNumber) {
+    // When guess is wrong
+  } else if (guess != secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high! 📈';
+      displayMessage(guess > secretNumber ? 'Too high! 📈' : 'Too low! 📈');
       score = score - 1;
-      document.querySelector('.score').textContent = score;
+      displayScore(score);
     } else {
-      document.querySelector('.message').textContent = 'You lost the game! 😥';
-      document.querySelector('.score').textContent = 0;
-    }
-
-    // When guess is to low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low! 📈';
-      score = score - 1;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game! 😥';
-      document.querySelector('.score').textContent = 0;
+      displayMessage('You lost the game! 😥');
+      displayScore(0);
     }
   }
 });
@@ -65,10 +62,10 @@ document.querySelector('.check').addEventListener('click', function () {
 document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('.number').textContent = '?';
+  displayMessage('Start guessing...');
+  displayNumber('?');
   document.querySelector('.guess').value = '';
-  document.querySelector('.score').textContent = score;
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
+  displayScore(score);
+  setBackgroundColor('#222');
+  numberStyle('15rem');
 });
